c9732885afdfb47766f274a69370a5f644a65513
Milad <slashmili@users.noreply.github.com>
2023-02-15 23:50:33 +0100

    add verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,7 @@ fi
 
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
+  flyctl --verbose launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
